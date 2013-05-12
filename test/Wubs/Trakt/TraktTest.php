53bddef03e3549638b0f3e9592bc56f1109577b6
megawubs <bwubs@me.com>
2013-05-13 01:17:57 +0200

    Added a test to test Post requests

--- test/Wubs/Trakt/TraktTest.php
+++ test/Wubs/Trakt/TraktTest.php
@@ -1,7 +1,13 @@
 <?php namespace Wubs\Trakt;
 
 class TraktTest extends \PHPUnit_Framework_TestCase{
+
 	public function testGetAccount(){
 		$this->assertInstanceOf('Wubs\Trakt\Account\Account', Trakt::get('account/settings'));
+	}
+
+	public function testPostSomething(){
+		$json = '{"username": "justin","password": "sha1hash","email": "username@gmail.com"}';
+		$this->assertInstanceOf('Wubs\Trakt\Account\Account', Trakt::post('account/create', $json));
 	}
 }
