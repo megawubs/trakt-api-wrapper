6794ac631c5a70de71bec37d8a17c073bd7ead7d
Bram Wubs <bwubs@me.com>
2013-05-30 20:13:54 +0200

    Made sure settings file is restored after test

--- tests/Wubs/Trakt/Account/AccountTest.php
+++ tests/Wubs/Trakt/Account/AccountTest.php
@@ -3,16 +3,39 @@
 use Wubs\Settings\Settings;
 
 class AccountTest extends \PHPUnit_Framework_TestCase{
+	
+	protected static $file;
+
+	protected static $content;
+
+	public static function setUpBeforeClass(){
+		self::$file = dirname(__FILE__).'/../../../../settings/settings.json';
+		self::$content = file_get_contents(self::$file);
+	}
+
+	public static function tearDownAfterClass(){
+		$s = new Settings();
+		$s->reset(self::$content);
+	}
+
+	public function setUp(){
+		$this->s = new Settings();
+		$this->account = new Account();
+	}
+
+	public function tearDown(){
+		unset($this->s);
+		unset($this->account);
+	}
 
 	public function testGetAccountInformation(){
-		// $this->markTestIncomplete('This test has not been implemented yet.');
-		$account = new Account();
-		$s = new Settings();
-		$username = $s->get('trakt.user');
-		$pass = sha1($s->get('trakt.pass'));
+		$username = $this->s->get('trakt.user');
+		$this->assertEquals('megawubs', $username);
+		$pass = $this->s->get('trakt.pass');
 		$params = '{"username":"'.$username.'","password":"'.$pass.'"}';
-		$account->setParams($params);
-		$res = $account->settings();
+		$account = $this->account->setParams($params);
+		$this->assertInstanceOf('Wubs\\Trakt\\Account\\Account', $account);
+		$res = $this->account->settings();
 		$this->assertArrayHasKey('status', $res);
 		$this->assertEquals('success', $res['status']);
 	}
