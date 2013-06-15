4cbf1a5443b13abd4844d83971bc3cfc792e5d7e
Bram Wubs <bwubs@me.com>
2013-06-15 16:05:25 +0200

    Added testPostTestRequest and testGetParams

--- tests/Wubs/Trakt/TraktTest.php
+++ tests/Wubs/Trakt/TraktTest.php
@@ -5,8 +5,8 @@ class TraktTest extends \PHPUnit_Framework_TestCase{
 
 	public function testPostAccount(){
 		$s = new Settings();
-		$username = $s->get('trakt.user');
-		$pass = sha1($s->get('trakt.pass'));
+		$username = $s->get('trakt.username');
+		$pass = sha1($s->get('trakt.password'));
 		$params = '{"username":"'.$username.'","password":"'.$pass.'"}';
 		$this->assertArrayHasKey('status', Trakt::post('account/settings', $params));
 	}
@@ -15,4 +15,18 @@ class TraktTest extends \PHPUnit_Framework_TestCase{
 		$json = '{"username": "justin","password": "sha1hash","email": "username@gmail.com"}';
 		$this->assertInstanceOf('Wubs\Trakt\Account\Account', Trakt::post('account/create', $json));
 	}
+ 
+	public function testGetParams(){
+		$params = Trakt::getParams(array('username', 'password'));
+		$this->assertInternalType('string', $params);
+	}
+
+	public function testPostTestRequest(){
+		$params = Trakt::getParams(array('username', 'password'));
+		$res = Trakt::post('account/test', $params);
+		$this->assertInternalType('array', $res);
+		$this->assertArrayHasKey('status', $res);
+		$this->assertEquals('success',$res['status']);
+	}
+
 }
