86e258054bf523c507c832882f32d9a90f0e672e
Bram Wubs <bwubs@me.com>
2013-06-16 22:42:03 +0200

    Added comments and made the setting method for quick trakt related settings access

--- src/Wubs/Trakt/Trakt.php
+++ src/Wubs/Trakt/Trakt.php
@@ -4,16 +4,35 @@ use Wubs\Settings\Settings;
 
 class Trakt{
 
+	/**
+	 * Maps a get request to the corresponding class and function
+	 * @param  string $request a api get request sting, all api methods 
+	 * can be found here: http://trakt.tv/api-docs
+	 * @return mixed
+	 */
 	public static function get($request){
 		$func = self::getFunc($request);
 		return self::getClass($request)->$func();
 	}
 
+	/**
+	 * Maps a post request, with the needed parameters
+	 * to the corresponding class and function
+	 * @param  string $request a api post request sting, all api methods 
+	 * can be found here: http://trakt.tv/api-docs
+	 * @return mixed
+	 */
 	public static function post($request, $params){
 		$func = self::getFunc($request);
 		return self::getClass($request, $params)->$func();
 	}
 
+	/**
+	 * Parses the class name from the request string
+	 * @param  string $request the request string
+	 * @param  string $params  the params if it's a post request
+	 * @return string          the name of the mapped class
+	 */
 	public static function getClass($request, $params = null){
 		$getList = explode('/', $request);
 		$className = __NAMESPACE__;
@@ -29,11 +48,23 @@ class Trakt{
 		return ($params == null ? $class : $class->setParams($params));
 	}
 
+	/**
+	 * Parses the function name from the request
+	 * @param  string $request the api request
+	 * @return string          the name of the function
+	 */
 	public static function getFunc($request){
 		$getList = explode('/', $request);
 		return end($getList);
 	}
 
+	/**
+	 * Gets the values for the parameter names you give it
+	 * example: $params Trakt::getParams(array('username', 'password'));
+	 * will return a valid json string that contains the username and password
+	 * @param  array $params list of the parameters to get
+	 * @return string         json string of the retrieved parameters
+	 */
 	public static function getParams(array$params){
 		$s = new Settings();
 		$returnParams = array();
@@ -47,7 +78,13 @@ class Trakt{
 		return json_encode($returnParams);
 	}
 
-	// public static function setting($string){
-
-	// }
+	/**
+	 * Shorthand for getting trakt related settings
+	 * @param  string $string the name of the setting
+	 * @return string         the value of the setting
+	 */
+	public static function setting($string){
+		$s = new Settings();
+		return $s->get('trakt.'$string);
+	}
 }
