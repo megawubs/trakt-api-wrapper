b5bb5f896d419cb8e556974891f2cc8a1a02e9a7
Bram Wubs <bwubs@me.com>
2013-06-16 22:27:49 +0200

    Added comments

--- src/Wubs/Trakt/Activity/Community.php
+++ src/Wubs/Trakt/Activity/Community.php
@@ -5,11 +5,18 @@ class Community extends Activity{
 
 	protected $typesSet = false;
 
+	/**
+	 * Sets the uri
+	 */
 	public function __construct(){
 		$this->setUri('activity/community.json');
 		$this->needsType = true;
 	}
 
+	/**
+	 * Sets the types part of the uri
+	 * @param array $types list of types
+	 */
 	public function setTypes(array$types){
 		$allowed = array('episode','show','movie','list');
 		$typesString = '/'.$this->filterToCSV($types, $allowed);
--- src/Wubs/Trakt/Activity/Episodes.php
+++ src/Wubs/Trakt/Activity/Episodes.php
@@ -2,20 +2,35 @@
 
 class Episodes extends Activity{
 
+	/**
+	 * Sets the uri
+	 */
 	public function __construct(){
 		$this->setUri('activity/episodes.json');
 	}
 
+	/**
+	 * Sets the show
+	 * @param string $showName comma separated show name(s)
+	 */
 	public function setShow($showName){
 		$this->appendUri("/$showName");
 		return $this;
 	}
 
+	/**
+	 * Sets the season
+	 * @param string $seasons comma separated season number(s)
+	 */
 	public function setSeasons($seasons){
 		$this->appendUri("/$seasons");
 		return $this;
 	}
 
+	/**
+	 * Sets the episodes
+	 * @param string $episodes comma separated episode number(s)
+	 */
 	public function setEpisodes($episodes){
 		$this->appendUri("/$episodes");
 		return $this;
