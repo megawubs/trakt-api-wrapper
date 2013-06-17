90ea9a367ce3364b5a1f0d70804edf708d42f59c
Bram Wubs <bwubs@me.com>
2013-06-18 01:06:08 +0200

    Deleted unused classes

--- src/Wubs/Trakt/Activity/Seasons.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Wubs\Trakt\Activity;
-
-class Movies extends Activity{
-
-	protected $uriOrder = array('title','season','actions','start_ts','end_ts');
-
-	protected $required = array('title', 'season');
-}
