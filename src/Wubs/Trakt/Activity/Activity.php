2c3e03e419ee15a5add2f5f10a70c37b82f6db08
Bram Wubs <bwubs@me.com>
2013-06-18 11:03:25 +0200

    Made functions for shows and user calls

--- src/Wubs/Trakt/Activity/Activity.php
+++ src/Wubs/Trakt/Activity/Activity.php
@@ -6,40 +6,78 @@ class Activity extends HttpBot{
 
 	/**
 	 * Runs when calling Trakt::get('activity/community');
-	 * @return Wubs\Trakt\Activity\Community 
-	 * Object with specific methods for the Community call
+	 * @return Wubs\Trakt\Activity 
+	 * Object
 	 */
 	public function community(){
-		$this->uriOrder = array('types', 'actions', 'start_ts', 'end_ts');
-		return $this->setUri('activity/community.json');
+		return $this->setUri('activity/community.json')
+		->setUriOrder(array('types', 'actions', 'start_ts', 'end_ts'));
 	}
 
 	/**
 	 * Runs when calling Trakt::get('activity/episodes');
-	 * @return Wubs\Trakt\Activity\Episodes 
-	 * Object with specific methods for the Episodes call
+	 * @return Wubs\Trakt\Activity
+	 * Object
 	 */
 	public function episodes(){
-		$this->uriOrder = array('title', 'season', 'episode', 'actions', 'start_ts', 'end_ts');
-		$this->required = array('title', 'season', 'episode');
-		return $this->setUri('activity/episodes.json');
+		return $this->setUri('activity/episodes.json')
+		->setUriOrder(array('title', 'season', 'episode', 'actions', 'start_ts', 'end_ts'))
+		->setRequired(array('title', 'season', 'episode'));
 	}
 
+	/**
+	 * Runs when calling Trakt::get('activity/friends');
+	 * @return Wubs\Trakt\Activity
+	 * Object
+	 */
 	public function friends(){
-		$this->uriOrder = array('types','actions','start_ts','end_ts');
-		return $this->setUri('activity/friends.json')->setHTTPType('post');
+		return $this->setUri('activity/friends.json')->setHTTPType('post')
+		->setUriOrder(array('types','actions','start_ts','end_ts'));
 	}
 
+	/**
+	 * Runs when calling Trakt::get('activity/movies');
+	 * @return Wubs\Trakt\Activity
+	 * Object
+	 */
 	public function movies(){
-		$this->uriOrder = array('title','actions','start_ts','end_ts');
-		$this->required = array('title');
-		return $this->setUri('activity/movies.json');
+		return $this->setUri('activity/movies.json')
+		->setUriOrder(array('title','actions','start_ts','end_ts'))
+		->setRequired(array('title'));
 	}
 
+	/**
+	 * Runs when calling Trakt::get('activity/seasons');
+	 * @return Wubs\Trakt\Activity
+	 * Object
+	 */
 	public function seasons(){
-		$this->uriOrder = array('title','season','actions','start_ts','end_ts');
-		$this->required = array('title', 'season');
-		return $this->setUri('activity/seasons.json');
+		return $this->setUri('activity/seasons.json')
+		->setUriOrder(array('title','season','actions','start_ts','end_ts'))
+		->setRequired(array('title', 'season'));
 	}
+
+	/**
+	 * Runs when calling Trakt::get('activity/shows');
+	 * @return Wubs\Trakt\Activity
+	 * Object
+	 */
+	public function shows(){
+		return $this->setUri('activity/shows.json')
+		->setUriOrder(array('title','actions','start_ts','end_ts'))
+		->setRequired(array('title'));
+	}
+
+	/**
+	 * Runs when calling Trakt::get('activity/user');
+	 * @return Wubs\Trakt\Activity
+	 * Object
+	 */
+	public function user(){
+		return $this->setUri('activity/user.json')
+		->setUriOrder(array('username','actions','start_ts','end_ts'))
+		->setRequired(array('username'));
+	}
+
 
 }
