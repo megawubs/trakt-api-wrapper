b8a36cb1ae6aca622666d8177418f14b1760962e
Bram Wubs <bwubs@me.com>
2013-06-19 01:19:02 +0200

    Moved uri mapping array to TraktUriOrder.php

--- src/Wubs/Trakt/Uri.php
+++ src/Wubs/Trakt/Uri.php
@@ -5,37 +5,7 @@ class Uri{
 
 	protected $uri = array();
 
-	protected $uriOrderAndRequiredList = array(
-		'account/create' => array(
-			'order' => array(),'required' => array())
-		,'account/settings' => array(
-			'order' => array(),'required' => array())
-		,'account/test' => array(
-			'order' => array(),'required' => array())
-		,'activity/community' => array(
-				'order' => array('types','actions','start_ts','end_ts'),'required' => array())
-		,'activity/episodes' => array(
-				'order' => array('title','season','episode','actions','start_ts','end_ts')
-				,'required' => array('title','season','episode'))
-		,'activity/friends' => array(
-				'order' => array('types','actions','start_ts','end_ts')
-				,'required' => array())
-		,'activity/movies' => array(
-				'order' => array('title','actions','start_ts','end_ts')
-				,'required' => array('title'))
-		,'activity/seasons' => array(
-			'order' => array('title', 'season','actions','start_ts','end_ts')
-			,'required' => array('title', 'season'))
-		,'activity/shows' => array(
-			'order' => array('title','actions','start_ts','end_ts')
-			,'required' => array('title'))
-		,'activity/user' => array(
-			'order' => array('username','actions','start_ts','end_ts')
-			,'required' => array('username'))
-		,'calendar/premieres' => array(
-			'order' => array('date','days')
-			,'required' => array())
-		);
+	protected $uriOrderAndRequiredList = array();
 
 	private $uriOrder = array();
 	
@@ -48,6 +18,9 @@ class Uri{
 	protected function setUriOrderAndRequired(){
 		$base = $this->uri['base'];
 		$base = str_replace('.json', '', $base);
+		$dir = dirname(__FILE__);
+		$this->uriOrderAndRequiredList = require $dir.'/TraktUriOrder.php';
+
 		if(array_key_exists($base, $this->uriOrderAndRequiredList)){
 			$this->setUriOrder($this->uriOrderAndRequiredList[$base]['order']);
 			$this->setRequired($this->uriOrderAndRequiredList[$base]['required']);
