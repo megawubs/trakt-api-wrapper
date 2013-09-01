805e27495b813883185f45af460bee1148a89bae
Bram Wubs <bwubs@me.com>
2013-09-01 03:14:00 +0200

    Refactor

--- src/Wubs/Trakt/Media/Media.php
+++ src/Wubs/Trakt/Media/Media.php
@@ -2,10 +2,20 @@
 
 
 use Wubs\Trakt\Base\TraktClass;
+use Wubs\Trakt\User;
 
 class Media extends TraktClass{
 
+	public $user;
+
+	protected $identifier;
+
 	public function __construct($identifier){
+		// parent::__construct();
+		$this->setIdentifier($identifier);
+	}
+
+	private function setIdentifier($identifier){
 		if(is_string($identifier)){
 			if(!strstr($identifier, '-')){
 				$identifier = str_replace(' ', '-', $identifier);
