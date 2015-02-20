094d136a53e9768274e831b1813a593ea46bedb8
Bram Wubs <bwubs@me.com>
2015-02-20 22:41:55 +0100

    Created TraktProvider and added dependancies

--- src/Wubs/Trakt/Trakt.php
+++ src/Wubs/Trakt/Trakt.php
@@ -1,77 +1,7 @@
 <?php namespace Wubs\Trakt;
 
-use Wubs\Trakt\Base\Uri;
-use Wubs\Trakt\Base\HttpBot;
-use Wubs\Trakt\User;
-use Wubs\Trakt\Media\Show;
 
 class Trakt
 {
 
-    public static $apiKey;
-
-    public static $version = '0.1';
-
-    /**
-     * Return a new show object
-     * @param  mixed $identifier Either the slug (i.e. the-walking-dead) or TVDB ID.
-     * @return Show             The show data mapped as a object
-     */
-    public static function show($identifier)
-    {
-        return new Show($identifier);
-    }
-
-    /**
-     * Create a new trakt user object
-     * @param  string $name A username from tratk.tv
-     * @param null $password
-     * @return User       A object containing the user data;
-     */
-    public static function user($name, $password = null)
-    {
-        return new User($name, $password);
-    }
-
-    /**
-     * Maps a get request to the corresponding class and function
-     * @param  string $request a api get request sting, all api methods
-     * can be found here: http://trakt.tv/api-docs
-     * @return HttpBot
-     */
-    public static function get($request)
-    {
-        return static::bot($request)->setHTTPType('get');
-    }
-
-    /**
-     * Maps a post request to the corresponding class and function
-     * @param  string $request a api post request sting, all api methods
-     * can be found here: http://trakt.tv/api-docs
-     * @return mixed
-     */
-    public static function post($request)
-    {
-        return static::bot($request)->setHTTPType('post');
-    }
-
-    /**
-     * Sets the api key for trakt
-     * @param string $key your trakt api key (see your account to find it)
-     */
-    public static function setApiKey($key)
-    {
-        static::$apiKey = $key;
-    }
-
-    /**
-     * returns a new instance of HttpBot with the uri set
-     * @param  string $uri the uri call to make (without the format part)
-     * @return HttpBot         an intstance of HttpBot
-     */
-    public static function bot($uri)
-    {
-        $uri = new Uri($uri, static::$apiKey);
-        return new HttpBot($uri);
-    }
 }
--- tests/Wubs/Trakt/Base/HttpBotTestTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HttpBotTestTest extends PHPUnit_Framework_TestCase {
-
-	public function testSetParamsAsArray(){
-		$uri = $this->getMock('Uri');
-		$uri->expects($this->once())->method('');
-	}
-}
