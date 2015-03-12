52bc1be754dde4f2bbf8990b7ad64c4d9b997062
Bram Wubs <bwubs@me.com>
2015-03-12 12:48:25 +0100

    Added calendars/movies response

---
+++ src/Wubs/Trakt/Response/Calendars/Movies.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 12/03/15
+ * Time: 12:39
+ */
+
+namespace Wubs\Trakt\Response\Calendars;
+
+
+use GuzzleHttp\Message\ResponseInterface;
+use Wubs\Trakt\Response\Response;
+
+class Movies implements Response
+{
+
+    public function handle(ResponseInterface $response)
+    {
+
+    }
+}
