500bf81705db1f810a73241805c10f3d5c50a07a
Bram Wubs <bwubs@me.com>
2015-03-15 18:39:07 +0100

    Implemented passing the ClientId and AccessToken from the request method

---
+++ src/Wubs/Trakt/Response/CheckIn.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 14/03/15
+ * Time: 14:18
+ */
+
+namespace Wubs\Trakt\Response;
+
+
+use League\OAuth2\Client\Token\AccessToken;
+use Wubs\Trakt\Media\Movie;
+
+class CheckIn
+{
+
+    /**
+     * @var Movie
+     */
+    public $movie;
+
+    private $sharing;
+    private $id;
+    /**
+     * @var AccessToken
+     */
+    private $token;
+
+    /**
+     * @param $json
+     * @param $id
+     * @param AccessToken $token
+     */
+    public function __construct($json, $id, AccessToken $token)
+    {
+        $this->movie = new Movie($json->movie, $id, $token);
+        $this->sharing = $json->sharing;
+
+        $this->id = $id;
+        $this->token = $token;
+    }
+
+    public function getMovie()
+    {
+        return $this->movie;
+    }
+
+    public function isSharedOnFacebook()
+    {
+        return $this->isSharedOn("facebook");
+    }
+
+    public function isSharedOnTwitter()
+    {
+        return $this->isSharedOn("twitter");
+    }
+
+    public function isSharedOnTumblr()
+    {
+        return $this->isSharedOn("tumblr");
+    }
+
+    private function isSharedOn($medium)
+    {
+        return $this->sharing->{$medium};
+    }
+}
