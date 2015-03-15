35f3f758c6baeb236941134736e2481b264867ed
Bram Wubs <bwubs@me.com>
2015-03-15 18:34:15 +0100

    Added wrapper for the ClientId

---
+++ src/Wubs/Trakt/ClientId.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 15/03/15
+ * Time: 17:50
+ */
+
+namespace Wubs\Trakt;
+
+
+use Wubs\Trakt\Request\Parameters\AbstractParameter;
+
+class ClientId extends AbstractParameter
+{
+
+}
