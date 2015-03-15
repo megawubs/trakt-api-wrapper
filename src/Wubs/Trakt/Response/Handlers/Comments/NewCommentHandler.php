f544945a70b3bdd00359d72a85f7cdf33a4c9826
Bram Wubs <bwubs@me.com>
2015-03-15 18:41:45 +0100

    Handler for posting a new comment

---
+++ src/Wubs/Trakt/Response/Handlers/Comments/NewCommentHandler.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 15/03/15
+ * Time: 17:08
+ */
+
+namespace Wubs\Trakt\Response\Handlers\Comments;
+
+
+use GuzzleHttp\Message\ResponseInterface;
+use Wubs\Trakt\Contracts\ResponseHandler;
+use Wubs\Trakt\Response\Handlers\AbstractResponseHandler;
+
+class NewCommentHandler extends AbstractResponseHandler implements ResponseHandler
+{
+
+    public function handle(ResponseInterface $response)
+    {
+
+    }
+}
