74bc12b10ce8972b8551950960b92386ade4898b
Bram Wubs <bwubs@me.com>
2015-03-21 09:40:01 +0100

    Added related request object

---
+++ src/Wubs/Trakt/Request/Movies/Related.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 21/03/15
+ * Time: 09:38
+ */
+
+namespace Wubs\Trakt\Request\Movies;
+
+
+use Wubs\Trakt\Request\AbstractRequest;
+use Wubs\Trakt\Request\Parameters\MediaId;
+use Wubs\Trakt\Request\RequestType;
+
+class Related extends AbstractRequest
+{
+
+    /**
+     * @var MediaId
+     */
+    private $id;
+
+    /**
+     * @param MediaId $id
+     */
+    public function __construct(MediaId $id)
+    {
+
+        $this->id = $id;
+    }
+
+    public function getRequestType()
+    {
+        return RequestType::GET;
+    }
+
+    /**
+     * @return MediaId
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUri()
+    {
+        return "movies/:id/related";
+    }
+}
