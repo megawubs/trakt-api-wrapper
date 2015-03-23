e14c0a89b704aed8732b19beca43c23b1c045e46
Bram Wubs <bwubs@me.com>
2015-03-23 11:45:32 +0100

    set expected json result

--- tests/Media/MovieTest.php
+++ tests/Media/MovieTest.php
@@ -11,21 +11,63 @@ use Wubs\Trakt\Request\Parameters\Query;
 class MovieTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testCanFindMovieOnSlug()
+    public function testCanMakeMovieObjectFromSearchResult()
     {
-        $movies = Movie::search(get_client_id(), get_token(), Query::set("guardians of the galaxy"));
+        $json = '{
+    "type": "movie",
+    "score": 26.019499,
+    "movie": {
+      "title": "Batman Begins",
+      "overview": "Driven by tragedy, billionaire Bruce Wayne dedicates his life to uncovering and defeating the corruption that plagues his home, Gotham City.  Unable to work within the system, he instead creates a new identity, a symbol of fear for the criminal underworld - The Batman.",
+      "year": 2005,
+      "images": {
+        "poster": {
+          "full": "https://walter.trakt.us/images/movies/000/000/001/posters/original/9634ffd477.jpg?1406080393",
+          "medium": "https://walter.trakt.us/images/movies/000/000/001/posters/medium/9634ffd477.jpg?1406080393",
+          "thumb": "https://walter.trakt.us/images/movies/000/000/001/posters/thumb/9634ffd477.jpg?1406080393"
+        },
+        "fanart": {
+          "full": "https://walter.trakt.us/images/movies/000/000/001/fanarts/original/7da8cfbe9e.jpg?1406080393",
+          "medium": "https://walter.trakt.us/images/movies/000/000/001/fanarts/medium/7da8cfbe9e.jpg?1406080393",
+          "thumb": "https://walter.trakt.us/images/movies/000/000/001/fanarts/thumb/7da8cfbe9e.jpg?1406080393"
+        }
+      },
+      "ids": {
+        "trakt": 1,
+        "slug": "batman-begins-2005",
+        "imdb": "tt0372784",
+        "tmdb": 272
+      }
+    }
+  }';
+        $clientId = get_client_id();
+        $token = get_token();
+        $mockResponse = new MockResponse($json);
 
-        $this->assertInstanceOf("Wubs\\Trakt\\Media\\Movie", $movies[0]);
+        $movie = new Movie($mockResponse->json(['object' => true]), $clientId, $token);
+
+        $this->assertInstanceOf("Wubs\\Trakt\\Media\\Movie", $movie);
+        $this->assertEquals(26.019499, $movie->score);
+        $this->assertEquals("Batman Begins", $movie->title);
     }
 
-    public function testReadCommentsFromMovie()
+    public function testCanMakeMovieObjectFromBasicResult()
     {
-        $movie = movie();
+        $json = '{
+    "title": "The Dark Knight",
+    "year": 2008,
+    "ids": {
+      "trakt": 16,
+      "slug": "the-dark-knight-2008",
+      "imdb": "tt0468569",
+      "tmdb": 155
+    }
+  }';
+        $mockResponse = new MockResponse($json);
+        $json = $mockResponse->json(["object" => true]);
 
-        $comments = $movie->comments();
+        $movie = new Movie($json, get_client_id(), get_token());
 
-        foreach ($comments as $comment) {
-            $this->assertInstanceOf("Wubs\\Trakt\\Response\\Comment", $comment);
-        }
+        $this->assertEquals("The Dark Knight", $movie->title);
     }
 }
