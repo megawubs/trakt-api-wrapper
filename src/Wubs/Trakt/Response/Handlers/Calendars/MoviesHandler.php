a723f16703366b118f85513b08af912b33810f5d
Bram Wubs <bwubs@me.com>
2015-03-23 11:45:55 +0100

    Deletions and renamed

--- src/Wubs/Trakt/Response/Handlers/Calendars/MoviesHandler.php
+++ src/Wubs/Trakt/Response/Handlers/Calendars/MoviesHandler.php
@@ -11,18 +11,24 @@ namespace Wubs\Trakt\Response\Handlers\Calendars;
 
 use GuzzleHttp\Message\ResponseInterface;
 use Wubs\Trakt\Contracts\ResponseHandler;
+use Wubs\Trakt\Request\Parameters\Days;
+use Wubs\Trakt\Request\Parameters\Type;
+use Wubs\Trakt\Response\Calendar\Calendar;
+use Wubs\Trakt\Response\Calendar\Day;
 use Wubs\Trakt\Response\Handlers\AbstractResponseHandler;
 use Wubs\Trakt\Response\Handlers\DefaultResponseHandler;
 
-class Movies extends AbstractResponseHandler implements ResponseHandler
+class MoviesHandler extends AbstractResponseHandler implements ResponseHandler
 {
 
     /**
      * @param ResponseInterface $response
-     * @return mixed
+     * @return Calendar
      */
     public function handle(ResponseInterface $response)
     {
-        return $this->getJson($response);
+        $json = $this->getJson($response);
+
+        return new Calendar($json, Type::movie(), $this->getId(), $this->getToken());
     }
 }
