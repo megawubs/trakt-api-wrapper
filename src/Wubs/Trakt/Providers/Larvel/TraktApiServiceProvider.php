89cb633affc311e4b8b50d78064545b19045a3bb
Bram Wubs <bwubs@me.com>
2015-09-17 10:40:09 +0200

    Added service provider and config file for laravel

---
+++ src/Wubs/Trakt/Providers/Larvel/TraktApiServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Wubs\Trakt\Providers\Larvel;
+
+
+use Illuminate\Support\ServiceProvider;
+use Wubs\Trakt\Auth\Auth;
+use Wubs\Trakt\Auth\TraktProvider;
+use Wubs\Trakt\Trakt;
+
+class TraktApiServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->publishes(
+            [
+                __DIR__ . 'trakt.php' => config_path('trakt.php'),
+            ]
+        );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            TraktProvider::class,
+            function ($app) {
+                return new TraktProvider(
+                    config("trakt.client_id"),
+                    config('trakt.client_secret'),
+                    config('trakt.trakt_redirect_uri')
+                );
+            }
+        );
+    }
+}
---
+++ src/Wubs/Trakt/Providers/Larvel/trakt.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "client_id" => getenv("CLIENT_ID"),
+    "client_secret" => getenv("CLIENT_SECRET"),
+    "redirect_uri" => getenv("TRAKT_REDIRECT_URI")
+];
