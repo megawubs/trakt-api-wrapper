dbf94b1c2a73c92591793e7e8ddb5bf8c5ac2858
Deejavu <DeeJaVu@users.noreply.github.com>
2016-10-17 16:32:34 +0200

    fixed incorrect key

--- src/Wubs/Trakt/Providers/Laravel/trakt.php
+++ src/Wubs/Trakt/Providers/Laravel/trakt.php
@@ -3,5 +3,5 @@
 return [
     "client_id" => getenv("CLIENT_ID"),
     "client_secret" => getenv("CLIENT_SECRET"),
-    "redirect_uri" => getenv("TRAKT_REDIRECT_URI")
+    "trakt_redirect_uri" => getenv("TRAKT_REDIRECT_URI")
 ];
